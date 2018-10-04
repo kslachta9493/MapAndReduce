a7f6da4de8d726982bb46e17dd112de5d8560a30
tusharma97 <tusharma97@gmail.com>
2018-10-04 03:17:03 -0400

    reading input

--- Map-Reduce/main.c
+++ Map-Reduce/main.c
@@ -10,6 +10,43 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    //printf("Hello, World!\n");
+    
+    if (argc!= 13){
+        printf("Invalid number of inputs");
+        return 1;
+    }
+    int type; //[0] for wordcount [1] sort
+    int impl; //[0] for procs [1] thread
+    int maps; // num of maps
+    int reduces; // num of reduces
+    char inFile[250]; // input file
+    char outFile[250]; // output file
+    
+    if(strcmp(argv[2],"wordcount") == 0){
+        type = 0;
+    }else{
+        type = 1;
+    }
+    
+    if(strcmp(argv[4],"procs") == 0){
+        impl = 0;
+    }else{
+        impl = 1;
+    }
+    
+    maps = atoi(argv[6]);
+    reduces = atoi(argv[8]);
+    strcpy(inFile, argv[10]);
+    strcpy(outFile, argv[12]);
+    
+    
+
+    
+    
+
+    
+    
     return 0;
-}
+    
+   }
