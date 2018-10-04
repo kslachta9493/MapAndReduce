37322b5633a35841a4c525802a7e7845c0205d73
tusharma97 <tusharma97@gmail.com>
2018-10-04 01:50:05 -0400

    Initial Commit

---
+++ Map-Reduce/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Map-Reduce
+//
+//  Created by Tushar Sharma on 10/4/18.
+//  Copyright © 2018 Tushar Sharma. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
